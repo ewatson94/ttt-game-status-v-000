8595c13a058b2247dc698d75402b19c7334080f3
Endya Watson <watson.endya@gmail.com>
2018-08-22 23:48:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_index[0, 1, 2, 3, 5, 6, 7] = win_combination[0]
+    [0, 1, 2, 3, 4, 5, 6, 7].any?
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
 
