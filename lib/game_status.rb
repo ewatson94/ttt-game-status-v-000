da8c7dedc4ac86b4b95085e4de2600f2a1825ea8
Endya Watson <watson.endya@gmail.com>
2018-08-22 23:59:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,8 +4,6 @@ def position_taken?(board, index)
 end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 def won?(board)
-  if position_taken(board, index) == false 
-    return false
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
@@ -16,7 +14,6 @@ def won?(board)
     position_3 = board[win_index_3] 
 
    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1).
-  end
   end
 end
  def full?(board)
