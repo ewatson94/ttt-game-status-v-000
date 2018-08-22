60ee0dd465fe202c9a8e624c8b5f0ca3aab4ad0d
Endya Watson <watson.endya@gmail.com>
2018-08-22 23:43:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,38 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+
+
+
+
+
+
+
+
+
+
+
+
+ def full?(board)
+  board.all? {|i| i == "X" || i == "O"}
+end 
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif !won?(board) && !full?(board)
+    return false
+  else won?(board)
+    return false
+  end
+end
+def over?(board)
+  if draw?(board) || won?(board) || full?(board)
+    return true
+  end
+end
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
